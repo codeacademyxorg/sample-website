57d6b49abc07807b056e72720756122c2c312ec7
jordivilagut <vilagutjordi@gmail.com>
2020-05-31 09:29:21 +0200

    Add media queries

--- scripts/noche.js
+++ scripts/noche.js
@@ -2,10 +2,10 @@ function pintarEstrellas() {
     
     var noche = $("#noche");
     
-    for (i = 0; i < 1000; i++) {
+    for (i = 0; i < 2000; i++) {
         
-        var coordenada1 = 50 + Math.random() * 1000;
-        var coordenada2 = Math.random() * 1000;
+        var coordenada1 = 50 + Math.random() * 3000;
+        var coordenada2 = Math.random() * 3000;
         var tiempo = Math.random() * 30;
         
         noche.append($("<div style='top:" + coordenada1 +"px; left: " + coordenada2 + "px; animation-duration: " + tiempo + "s;'></div>").addClass("estrella"));
